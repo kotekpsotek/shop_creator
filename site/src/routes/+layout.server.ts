b6f749c4abe6a95ec81be6fb7b9bba2daf4d45d9
Michał Szczepański <siematumichael@protonmail.com>
2023-09-17 16:01:58 +0200

    Application upper stripe layout with login and signup functionalities

---
+++ site/src/routes/+layout.server.ts
@@ -0,0 +1,2 @@
+/* FROM THIS VARIABLE SET AS 'FALSE' whole application is SPA */
+export const ssr = false;
---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { UserAvatar, OverflowMenuHorizontal } from "carbon-icons-svelte";
+    import { navigating } from "$app/stores";
+    import { goto } from "$app/navigation"
+
+    const url = new URL(document.URL);
+
+    let accountBiblord = false;
+</script>
+
+{#key $navigating}
+    <!-- TODO: Styles -->
+    {#if !new URL(document.URL).pathname.includes("layouts_preview")}
+        <div class="top-most-worth">
+            <div class="menu">
+                <OverflowMenuHorizontal/>
+            </div>
+            <p>My shop creator</p>
+            <button class="account" on:click={_ => accountBiblord = !accountBiblord}>
+                <UserAvatar/>
+            </button>
+        </div>
+        {#if accountBiblord}
+            <div class="account-bilboard">
+                <button on:click={_ => goto("/account/login")}>Login</button>
+                <button on:click={_ => goto("/account/create")}>Create account</button>
+            </div>
+        {/if}
+    {/if}
+{/key}
+<slot/>
+
+<style>
+    .top-most-worth {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
