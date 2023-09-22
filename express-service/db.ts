488a7b676c902e3f814fa7a72feef3ab80bdd42f
Michał Szczepański <siematumichael@protonmail.com>
2023-09-22 17:24:05 +0200

    Connect Application with Cassandra NoSQL Distributed, Reliable database

--- express-service/db.ts
+++ express-service/db.ts
@@ -1,11 +1,20 @@
 import { createConnection, Schema, SchemaTypes } from "mongoose";
 import { randomUUID } from "crypto"
 import * as redis from "redis";
+import cassandra from "cassandra-driver";
 
 // Conns
 export const mDb = createConnection("mongodb://127.0.0.1:27017/service_shop_creator");
 export const rDb = redis.createClient({ url: "redis://127.0.0.1:6379" });
 rDb.connect();
+export const cDb = new cassandra.Client({
+    keyspace: "shop_creator",
+    contactPoints: ["127.0.0.1"],
+    localDataCenter: "datacenter1",
+});
+cDb.connect()
+
+cDb.execute("DESC tables;").then(v => console.table(v.rows))
 
 // Schemas
 export interface User {
