359d43c210a308d9a8aaee2c7e18f2009b74b3ca
Michał Szczepański <siematumichael@protonmail.com>
2023-09-22 16:20:41 +0200

    Finished step 3 for creating shop stage

--- site/src/routes/preparation/+page.svelte
+++ site/src/routes/preparation/+page.svelte
@@ -4,20 +4,45 @@
     import { goto } from "$app/navigation";
     import RemarkableWhiteBlackLayout from "$lib/layouts/remarkableWhiteBlack_layout.svelte";
     import { Stepper, Step } from '@skeletonlabs/skeleton';
-    import stripeLogo from "$lib/stripe-logo.svg"
+    import stripeLogo from "$lib/stripe-logo.svg";
+    import { Spinner } from "flowbite-svelte";
     
     type FashionLayouts = "remarkable-blackwhite";
     
     let stepNumber = 2;
     $: stepName = stepNumber == 0 ? "Prepare Foundaments" : (stepNumber == 1 ? "Prepare Layout" : (stepNumber == 2 ? "Billing preparation" : "Preparation Finalization"));
 
-    let alertDisp = false;
-    onMount(() => {
+    let stripeConnectSate: { notAllow: boolean, m: string } | undefined;
+    let loading: { s: true, message: string } | undefined;
+    /** Handle redirection from stripe */
+    async function redirectFromStripeHandle() {
         const u = new URL(document.URL);
-        if (u.searchParams.has("return")) {
-            // TODO: Check
+        if (u.search == "?return") { // Verify link which suggest that user ends account connection (whether data was harvested in whole span of 'KYC' term)
+            loading = { s: true, message: "Checking whether you enclose Stripe Connect to your account" };
+            const f = await fetch("http://localhost:8100/payments/connect-shop", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    'content-type': "application/json"
+                },
+                body: JSON.stringify({ verify: true })
+            });
+
+            loading = undefined;
+            if (f.status != 200) {
+                stripeConnectSate = { notAllow: true, m: "Stripe and we couldn't collect all required informations to perform payouts for you" };
+            }
+            else stripeConnectSate = { notAllow: false, m: "" };
+        } 
+        else if (u.search == "?refresh") { // Refresh link which e.g: expired
+            // Same function for both action is more simple
+            await connectWithStripe();
         }
-    });
+    }
+    // onMount(redirectFromStripeHandle);
+    $: if (stepNumber == 2) {
+        redirectFromStripeHandle();
+    }
     
     let shopTypeIsInSelecting = false;
     let shopType: undefined | string;
@@ -35,6 +60,7 @@
         showUnselectCapability;
         files;
         pickupLayout;
+        stripeConnectSate;
         locked = !allowGoAhead();
     }
 
@@ -44,8 +70,9 @@
             const stL = (shopTitle?.trim() || "").length || 0;
             return (shopType?.length || 0) != 0 && (stL >= 5 && stL <= 30) && (files || []).length != 0;
         } else if (stepNumber == 1) {
-            console.log("al", typeof pickupLayout != "undefined")
             return typeof pickupLayout != "undefined";
+        } else if (stepNumber == 2) {
+            return (stripeConnectSate && !stripeConnectSate.notAllow) ? true : false;
         }
 
         return false;
@@ -111,7 +138,13 @@
         };
     }
 
+    /** Connect user with stripe by govern it's datas */
     async function connectWithStripe() {
+        // Reset likely filled 'stripeConnectSate' datas
+        stripeConnectSate = undefined;
+        loading = { s: true, message: "Redirecting to Stripe Connect Service" };
+        
+        // Src action
         const bck = await fetch("http://localhost:8100/payments/connect-shop", {
             method: "post",
             credentials: "include",
@@ -120,13 +153,40 @@
         if (bck.status == 200) {
             const { r_url } = await bck.json();
             window.location.assign(r_url);
+            loading = undefined;
         }
         else alert("Cannot perform 'connect account action' via Stripe")
     }
+
+    function completeShopCreation() {
+        // TODO: Save shop datas, Redirect to final page
+    }
 </script>
 
+{#if loading?.s}
+    <!-- Display loading element -->
+    <div class="absolute top-0 right-0 w-full h-full bg-sky-950 flex items-center justify-center">
+        <div class="card variant-soft-surface p-4 h-1/5 w-2/4 flex flex-col gap-y-5 justify-center items-center">
+            <Spinner/>
+            <p class="font-bold text-lg">{loading.message}</p>
+        </div>
+    </div>
+{/if}
+{#if stripeConnectSate  && stripeConnectSate.notAllow}
+    <!-- When user must again attach all datas to his Stripe Connect account -->
+    <aside class="alert variant-filled-secondary">
+        <!-- Icon -->
+        <div>
+            🔎
+        </div>
+        <!-- Message -->
+        <div class="alert-message">
+            {stripeConnectSate.m}
+        </div>
+    </aside>
+{/if}
 <div class="bckg p-5">
-    <Stepper start={2} on:next={goToNextStep} on:back={goToPreviousStep}>
+    <Stepper start={2} on:next={goToNextStep} on:back={goToPreviousStep} on:complete={completeShopCreation}>
         <Step {locked}>
             <svelte:fragment slot="header">
                 <h2 class="badge variant-soft-secondary font-normal w-fit gap-y-5">{stepName}</h2>
@@ -213,128 +273,22 @@
                         is platform by which we make all payments and payouts
                     </p>
                 </div>
-                <button class="btn variant-soft-tertiary w-60" on:click={connectWithStripe}>
-                    <p>Connect with</p>
-                    <img src="{stripeLogo}" alt="" class="w-14 h-8">
-                    <p>now</p>
-                </button>
+                {#key stripeConnectSate}
+                    <button class="btn variant-soft-tertiary w-60" disabled={allowGoAhead()} on:click={connectWithStripe}>
+                        {#if allowGoAhead()}
+                            <p>You're connected with</p>
+                            <img src="{stripeLogo}" alt="" class="w-14 h-8">
+                        {:else}
+                            <p>Connect with</p>
+                            <img src="{stripeLogo}" alt="" class="w-14 h-8">
+                            <p>now</p>
+                        {/if}
+                    </button>
+                {/key}
             </div>
-            {#if alertDisp}
-                <aside class="alert variant-filled-secondary">
-                    <!-- Icon -->
-                    <div>
-                        🔎
-                    </div>
-                    <!-- Message -->
-                    <div class="alert-message">
-                        
-                    </div>
-                </aside>
-            {/if}
         </Step>
         <!-- ... -->
     </Stepper>
-    <div class="setps flex flex-col mt-5" style="display: none;">
-        <h2 class="badge variant-soft-secondary font-normal w-fit gap-y-5">{stepName}</h2>
-        <div class="cmn-ac">
-            {#if stepNumber == 1}
-                <div class="slel">
-                    {#if !shopTypeIsInSelecting && !shopType}
-                        <button on:click={_ => {
-                            shopTypeIsInSelecting = true;
-                        }}>
-                            Add Shop Type
-                        </button>
-                    {:else}
-                        <select name="sl-type" on:blur={_ => shopTypeIsInSelecting = false} bind:value={shopType}>
-                            <option value="fashion">Fashion Store</option>
-                            <option value="supplements">Supplements Store</option>
-                        </select>
-                    {/if}
-                </div>
-                <input type="text" placeholder="Add Shop Title" minlength="5" maxlength="30" bind:value={shopTitle}>
-                <div class="lgl">
-                    {#if !files || files.length == 0}
-                        <button class="logo" on:click={selectLogo}>
-                            <Image size={32}/>
-                            <input type="file" accept="image/png, image/jpeg, image/svg+xml, image/webp, image/gif" multiple={false} hidden bind:files/>
-                        </button>
-                    {:else}
-                        <button on:click={unselectLogo} on:mouseenter={_ => showUnselectCapability = true} on:mouseleave={_ => showUnselectCapability = false}>
-                            {#if showUnselectCapability}
-                                <div class="unselect-cap">
-                                    <p>Click to unselect image</p>
-                                </div>
-                            {/if}
-                            <img src="" alt="" use:loadLogoPreview>
-                        </button>
-                    {/if}
-                </div>
-            {:else if stepNumber == 2}
-                <h1 class="h1 font-bold">Layouts showcase</h1>
-                <div class="ls mt-5">
-                    <button class="p-2 card-hover variant-ringed rounded cursor-pointer flex flex-col gap-y-2 h-fit hover:variant-soft-primary" on:click={pickLayout("remarkable-blackwhite")} on:dblclick={_ => goto("/layouts_preview/remarkable-whiteblack")}>
-                        <p class="p text-xl font-serif font-normal">Remarkable White Black</p>
-                        <div class="preview flex flex-col gap-y-2">
-                            <div class="w-full">
-                                <p class="text-start text-base font-normal bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone capitalize">Short preview:</p>
-                            </div>
-                            <div class="p h-96">
-                                <RemarkableWhiteBlackLayout previewMode={true}/>
-                            </div>
-                        </div>
-                    </button>
-                    <button class="p-2 card-hover variant-ringed rounded cursor-pointer flex flex-col gap-y-2 h-fit hover:variant-soft-primary" on:click={pickLayout("remarkable-blackwhite")} on:dblclick={_ => goto("/layouts_preview/remarkable-whiteblack")}>
-                        <p class="p text-xl font-serif font-normal">Remarkable White Black</p>
-                        <div class="preview flex flex-col gap-y-2">
-                            <div class="w-full">
-                                <p class="text-start text-base font-normal bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone capitalize">Short preview:</p>
-                            </div>
-                            <div class="p h-96">
-                                <RemarkableWhiteBlackLayout previewMode={true}/>
-                            </div>
-                        </div>
-                    </button>
-                    <button class="p-2 card-hover variant-ringed rounded cursor-pointer flex flex-col gap-y-2 h-fit hover:variant-soft-primary" on:click={pickLayout("remarkable-blackwhite")} on:dblclick={_ => goto("/layouts_preview/remarkable-whiteblack")}>
-                        <p class="p text-xl font-serif font-normal">Remarkable White Black</p>
-                        <div class="preview flex flex-col gap-y-2">
-                            <div class="w-full">
-                                <p class="text-start text-base font-normal bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone capitalize">Short preview:</p>
-                            </div>
-                            <div class="p h-96">
-                                <RemarkableWhiteBlackLayout previewMode={true}/>
-                            </div>
-                        </div>
-                    </button>
-                    <button class="p-2 card-hover variant-ringed rounded cursor-pointer flex flex-col gap-y-2 h-fit hover:variant-soft-primary" on:click={pickLayout("remarkable-blackwhite")} on:dblclick={_ => goto("/layouts_preview/remarkable-whiteblack")}>
-                        <p class="p text-xl font-serif font-normal">Remarkable White Black</p>
-                        <div class="preview flex flex-col gap-y-2">
-                            <div class="w-full">
-                                <p class="text-start text-base font-normal bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone capitalize">Short preview:</p>
-                            </div>
-                            <div class="p h-96">
-                                <RemarkableWhiteBlackLayout previewMode={true}/>
-                            </div>
-                        </div>
-                    </button>
-                </div>
-            {:else if stepNumber == 3}
-                <!-- Payments support bank card code needed -->
-                
-            {/if}
-        </div>
-        <!-- <div class="an-ac">
-            <p>{stepNumber}/3</p>
-            {#key (shopType && shopTitle && files) || pickupLayout}
-                {#if stepNumber != 3}
-                    <button disabled={!allowGoAhead()} on:click={goToNextStep}>Next</button>
-                {/if}
-            {/key}
-            {#if stepNumber != 1}
-                <button on:click={goToPreviousStep}>Previous</button>
-            {/if}
-        </div> -->
-    </div>
 </div>
 
 <style>
--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -3,29 +3,50 @@ import type { Config } from 'tailwindcss';
 
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
+import flowbite from "flowbite/plugin";
 
 // Import Forms plugin for tailwindcss
 import forms from "@tailwindcss/forms"
 
+const contentSkeleton = [
+	'./src/**/*.{html,js,svelte,ts}',
+	// 3. Append the path to the Skeleton package
+	join(require.resolve(
+		'@skeletonlabs/skeleton'),
+		'../**/*.{html,js,svelte,ts}'
+	)
+];
+const contentFlowbite = ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'];
+
 const config = {
 	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
+	content: [...contentSkeleton, ...contentFlowbite],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				// flowbite-svelte
+				flowbite: {
+					50: '#FFF5F2',
+					100: '#FFF1EE',
+					200: '#FFE4DE',
+					300: '#FFD5CC',
+					400: '#FFBCAD',
+					500: '#FE795D',
+					600: '#EF562F',
+					700: '#EB4F27',
+					800: '#CC4522',
+					900: '#A5371B'
+				  }
+			}
+		},
 	},
 	plugins: [
 		skeleton({
 			themes: { preset: [ "wintry" ] }
 		}),
-		forms
+		forms,
+		flowbite
 	]
 } satisfies Config;
 
