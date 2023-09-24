f342b4105e2489552c1e068b047c2452c3dec5aa
Michał Szczepański <siematumichael@protonmail.com>
2023-09-24 17:00:46 +0200

    Finish page for shop creation action

---
+++ site/src/routes/preparation/final/+page.svelte
@@ -0,0 +1,9 @@
+<div class="w-screen h-screen flex justify-center items-center">
+    <div class="card variant-ghost-success p-4">
+        <h2 class="h2 font-bold mb-2">Congratulations, you finish!</h2>
+        <p> Your shop is ready! Nevertheless don't forget about your shop preparation</p>
+        <a class="anchor" href="/user-shop-panel/">
+            Let visit your shop panel
+        </a>
+    </div>
+</div>
