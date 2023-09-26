80685f5eecd9694baac72b247868ea84cda79448
Michał Szczepański <siematumichael@protonmail.com>
2023-09-26 16:31:29 +0200

    Testing RabbitMQ AMQPLIB node.js library over vitest unit testing library

---
+++ express-service/tests/main.test.ts
@@ -0,0 +1,30 @@
+import amqp from "amqplib";
+import { describe, test, beforeAll } from "vitest";
+import { PacketMq } from "../queue-streaming"
+
+async function createConnection() {
+    const connection = await amqp.connect("amqp://localhost:5762");
+    const channel = await connection.createChannel();
+
+    return { connection, channel };
+};
+
+const queueName = "messages";
+
+beforeAll(async () => {
+    const { channel } = await createConnection();
+
+    channel.consume(queueName, async (msg) => {
+        // channel.deleteQueue(queueName)
+        console.log(`Message received ${msg?.content}`);
+    })
+});
+
+test("RabbitMQ queue send message", async () => {
+    const { channel } = await createConnection();
+
+    const message = "1st message";
+
+    await channel.assertQueue(queueName, { durable: false, arguments: {} });
+    channel.sendToQueue(queueName, Buffer.from(message));
+})
